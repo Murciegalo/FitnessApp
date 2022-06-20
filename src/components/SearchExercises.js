9a432b299e62bb7042e90b89e6767b11735a2e67
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-06-20 10:14:45 +0200

    SearchExercises comp. in progress

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useState, useEffect } from 'react';
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  return (
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      <Typography
+        fontWeight='700'
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb='50px'
+        textAlign='center'>
+        Awesome Exercises <br />
+        you should know
+      </Typography>
+      <Box></Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
