b7b31cfb5da4f9202e648e282317eeca64b36327
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-06-28 12:44:16 +0200

    exerciseDetail_Pg and related comps. in progress

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,12 +1,27 @@
-import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { exercisesOptions, fetchData } from '../utils/fetchData';
+import { Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  const param = useParams();
+  const { id } = useParams();
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  useEffect(() => {
+    console.log(process.env.REACT_APP_EXERCISE_DB_URL);
+    const fetchExercisesData = async () => {};
+    fetchExercisesData();
+  }, [id]);
 
-  return <Box>ExerciseDetail</Box>;
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
